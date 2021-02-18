b00cacb9595e3a7afacb752f8f9a05f9a08b11f3
Natália Viveiros <nvmoreira@id.uff.br>
2021-02-18 20:03:03 -0300

    primeiro commit

---
+++ src/main/java/br/com/zup/sitedeviagens/SiteDeViagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.sitedeviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteDeViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteDeViagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/sitedeviagens/SiteDeViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.sitedeviagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteDeViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
